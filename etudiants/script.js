801519e48fdc2a1480a59f0abf1cba24278f2a57
enaanai <aenaanai@uae.ac.ma>
2024-04-19 19:58:05 +0100

    modal

--- etudiants/script.js
+++ etudiants/script.js
@@ -5,9 +5,32 @@ window.addEventListener('load',()=>{
         id=$('<td>').text(etudiant.id)
         nom=$('<td>').text(etudiant.nom)
         age=$('<td>').text(etudiant.age)
+        action=$('<td>')
         ligne=$('<tr>')
         ligne.append(id,nom,age)
         $('#listeEtudiants').append(ligne)
        })
     })
 })
+
+function ajouterEtudiant(){
+formData={}
+formData["id"]=$("#id").val()
+formData["nom"]=$("#nom").val()
+formData["age"]=$("#age").val()
+jsonData=JSON.stringify(formData)
+$.post("http://localhost:8080/SupTechBackEnd/etudiants", jsonData,
+    function (data, textStatus, jqXHR) {
+        if (data!=null){
+            id=$('<td>').text(formData["id"])
+            nom=$('<td>').text(formData["nom"])
+            age=$('<td>').text(formData["age"])
+            ligne=$('<tr>')
+            ligne.append(id,nom,age)
+            $('#listeEtudiants').append(ligne)
+        }
+    },
+    "json"
+);
+        
+}
