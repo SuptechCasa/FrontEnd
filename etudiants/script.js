3e4950af2d53252f90e673b5c483cfb522352c54
enaanai <aenaanai@uae.ac.ma>
2024-05-13 14:44:47 +0100

    deleteSansModal

--- etudiants/script.js
+++ etudiants/script.js
@@ -5,9 +5,15 @@ window.addEventListener('load',()=>{
         id=$('<td>').text(etudiant.id)
         nom=$('<td>').text(etudiant.nom)
         age=$('<td>').text(etudiant.age)
+        deleteTD=$('<td>')
+        deleteIMG=$('<img>')
+        deleteIMG.attr('src','images/delete.png')
+        deleteIMG.attr('class','deleteImage')
+        deleteIMG.attr('onclick','deleteEtudiant(event)')
+        deleteTD.append(deleteIMG)
         action=$('<td>')
         ligne=$('<tr>')
-        ligne.append(id,nom,age)
+        ligne.append(id,nom,age,deleteTD)
         $('#listeEtudiants').append(ligne)
        })
     })
@@ -25,12 +31,39 @@ $.post("http://localhost:8080/SupTechBackEnd/etudiants", jsonData,
             id=$('<td>').text(formData["id"])
             nom=$('<td>').text(formData["nom"])
             age=$('<td>').text(formData["age"])
+            tdIMG=$('<td>').html("<img class='deleteImage' src='images/delete.png'>")
             ligne=$('<tr>')
-            ligne.append(id,nom,age)
+            ligne.append(id,nom,age,tdIMG)
             $('#listeEtudiants').append(ligne)
         }
     },
     "json"
 );
         
+}
+
+function deleteEtudiant(e) {
+    const id = e.target.parentElement.parentElement.children[0].innerText;
+    const nom = e.target.parentElement.parentElement.children[1].innerText;
+    const age = e.target.parentElement.parentElement.children[2].innerText;
+
+    const formData = {
+        id: parseInt(id),
+        nom: nom,
+        age: parseInt(age)
+    };
+    console.log(JSON.stringify(formData))
+    $.ajax({
+        url: 'http://localhost:8080/SupTechBackEnd/etudiants',
+        type: 'DELETE',
+        contentType: 'application/json', // Définir le type de contenu pour JSON
+        data: JSON.stringify(formData), // Convertir l'objet formData en JSON
+        success: function(response) {
+            e.target.parentElement.parentElement.remove()
+        },
+        error: function(xhr, status, error) {
+            // Handle error
+            console.error('DELETE request failed:', error);
+        }
+    });
 }
